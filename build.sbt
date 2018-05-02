29a42482879c4d77012143d80cd94c7f7eac23e4
diggzhang <diggzhang@gmail.com>
2018-05-02 16:57:37 +0800

    Add json parse package into build.sbt

--- build.sbt
+++ build.sbt
@@ -7,5 +7,6 @@ scalaVersion := "2.12.2"
 libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-http"   % "10.1.1",
   "com.typesafe.akka" %% "akka-stream" % "2.5.11",
-  "ch.megard" %% "akka-http-cors" % "0.3.0"
+  "ch.megard" %% "akka-http-cors" % "0.3.0",
+  "com.typesafe.play" %% "play-json" % "2.6.9"
 )
--- src/main/scala/com/guanghe/controllers/DataProcessing.scala
+++ src/main/scala/com/guanghe/controllers/DataProcessing.scala
@@ -6,12 +6,14 @@ import akka.http.scaladsl.server.Directives._
 class DataProcessing {
 
   def dataProcessing(request: HttpRequest, apiName: String) = {
+    val timeStart: Long = System.currentTimeMillis()
+    val mergeIpUaJsonRaw = extractUaIpInReqHeader(request)
+
     entity(as[Array[Byte]]) {
       entityByte => {
-        ungzip
+        complete(StatusCodes.NoContent)
       }
     }
   }
-
 
 }
