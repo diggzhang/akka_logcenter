ffb379c4d411a4f063af13ca7d2e62327514bfb3
diggzhang <diggzhang@gmail.com>
2018-05-02 15:15:55 +0800

    Add API log route

---
+++ src/main/scala/com/guanghe/routes/ApiLogRoute.scala
@@ -0,0 +1,20 @@
+package com.guanghe.routes
+
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+
+class ApiLogRoute {
+  val route: Route =
+    post {
+      pathPrefix("api" / "v4" / "httplog") {
+        pathEndOrSingleSlash {
+          entity(as[String]) {
+            json: String => {
+              complete(StatusCodes.NoContent)
+            }
+          }
+        }
+      }
+    }
+}
