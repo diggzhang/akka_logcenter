149df149aba8286c84e99071e02e48f1caf8c6ab
diggzhang <diggzhang@gmail.com>
2018-05-02 15:34:18 +0800

    Add log api

--- src/main/scala/com/guanghe/routes/ApiLogRoute.scala
+++ src/main/scala/com/guanghe/routes/ApiLogRoute.scala
@@ -15,6 +15,16 @@ class ApiLogRoute {
             }
           }
         }
+      } ~
+      pathPrefix("api" / "log") {
+        pathEndOrSingleSlash {
+          extractRequest {
+            request =>
+              {
+                complete(StatusCodes.NoContent)
+              }
+          }
+        }
       }
     }
 }
